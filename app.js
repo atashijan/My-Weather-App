3ced973b63474cd149f0786b7fbeff1869270dab
a-tashijan <nunetashijan55@gmail.com>
2023-04-11 13:58:52 -0400

    updated html

---
+++ app.js
@@ -0,0 +1,64 @@
+let now = new Date();
+
+let currentDay = document.querySelector("#day");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+currentDay.innerHTML = `${day}`;
+
+let currentMonth = document.querySelector("#month");
+let date = now.getDate();
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+let thisMonth = months[now.getMonth()];
+
+currentDay.innerHTML = `${day}`;
+currentMonth.innerHTML = `${thisMonth} ${date}`;
+
+let currentTime = document.querySelector("#time");
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+currentTime.innerHTML = `${hours}:${minutes}`;
+
+function displayWeather(response) {
+  console.log(response.data);
+  document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("h2").innerHTML = Math.round(response.data.main.temp);
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "a9c42cfab3ac3bfe20bb56fcf9038bca";
+  let city = document.querySelector("#search-city-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeather);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
